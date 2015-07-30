860cbed7ddbfbc384680d1167319c96da04b6752
Ishwor Thapaliya <ishwor.thapaliya@elido.fi>
2015-07-30 10:46:40 +0300

    added a new response

---
+++ api/responses/emailAddressInUse.js
@@ -0,0 +1,8 @@
+module.exports = function emailAddressInUse (){
+
+  // Get access to `res`
+  // (since the arguments are up to us)
+  var res = this.res;
+
+  return res.send(409, 'Email address is already taken by another user.');
+};
